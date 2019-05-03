2c77f603e6b7069bf677f928afa10dcbd8f40b74
yassir ramdani <yassir.ramdani@epita.fr>
2019-05-03 02:04:35 +0200

    Initial Commit

---
+++ CardiodDraw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardiodDraw
+//
+//  Created by Yassir RAMDANI on 5/3/19.
+//  Copyright © 2019 Yassir RAMDANI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
